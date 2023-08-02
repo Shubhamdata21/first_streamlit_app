939cc30f4127fc3c98630dd4f5cf6038701a89b7
Shubhamdata21 <141220368+Shubhamdata21@users.noreply.github.com>
2023-08-02 15:20:50 +0530

    Update streamlit_app.py
    UTF Character error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('[My parents New Healthy Dinner]')🥣 🥗 🐔 🥑🍞
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-stremlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+stremlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
