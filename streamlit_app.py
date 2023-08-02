150c40c86c8d9fc76bcc4a211f215ad779e4e4a7
Shubhamdata21 <141220368+Shubhamdata21@users.noreply.github.com>
2023-08-02 15:20:06 +0530

    Update streamlit_app.py
    Added and updated emoji and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title('[My parents New Healthy Dinner]')
-streamlit.header('[Breakfast Menu]')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale Spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled Free Range Eggs')
+streamlit.title('[My parents New Healthy Dinner]')🥣 🥗 🐔 🥑🍞
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+stremlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
